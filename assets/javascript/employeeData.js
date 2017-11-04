b991180f80dae73f6482777e0401b3214a8d23bd
Anisha Patel <patella08@yahoo.com>
2017-11-04 12:42:08 -0400

    Added Charles Code

--- assets/javascript/employeeData.js
+++ assets/javascript/employeeData.js
@@ -12,17 +12,6 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-const tableHeader = `
-	<tr>
-		<th>Employee Name</th>
-		<th>Role</th>
-		<th>Start Date</th>
-		<th>Months Worked</th>
-		<th>Monthly Rate $</th>
-		<th>Total Billed $</th>
-	</tr>
-`
-
 
 // ---------------------------------------------------------------------------------------------------------------
 // On "Add" button click process
@@ -47,6 +36,7 @@ $("#form").submit(function(e) {
 
 });
 
+// create and format data to table row
 function createTableRow(name, role, start, monthsWorked, monthlyRate, billed) {
 	return `
 		<tr>
@@ -60,22 +50,35 @@ function createTableRow(name, role, start, monthsWorked, monthlyRate, billed) {
 	`;
 }
 
-// for page load
+// calculate months worked
+function calculateMonthsWorked(start) {
+	// initialize momentjs date with firebase utc timestamp
+	var startDate = moment(start);
+	// initialize momentjs now date
+	var todaysDate = moment();
+	return todaysDate.diff(startDate, 'months', true);
+}
+
+// populate table on page load
 database.ref('employees').orderByChild('dateAdded').once('value', function(data) {
 	console.log('ran the once')
-	let fragment = tableHeader;
+	let fragment = '';
+	// get rows from firebase row entries
 	var rows = Object.values(data.val())
+	// iterate over row entries
 	for (var row of rows) {
-		// get data
+		// deconstruct row properties
 		var name = row.name;
 		var role = row.role;
 		var start = row.startDate;
 		var monthlyRate = row.monthlyRate;
-		var billed = 50;
-		// get months worked
+		// calculate months worked
 		var today = new Date();
 		var todayDate = today.getDate();
 		var monthsWorked = 10;
+		// calculate total billed
+		var billed = monthsWorked * monthlyRate;
+		// create row and append to fragment
 		fragment += createTableRow(
 			name,
 			role,
@@ -85,13 +88,17 @@ database.ref('employees').orderByChild('dateAdded').once('value', function(data)
 			billed
 		)
 	}
-	$('#table').html(fragment)
+	// add fragment to table
+	$('#table').append(fragment)
 })
 
-// on row inserted
-database.ref('employees').orderByChild('dateAdded').limitToLast(1).on('value', function(data) {
-	var row = Object.values(data.val())[0]
-	// get data
+// populate new table rows on row added
+database.ref('employees').orderByChild('dateAdded').limitToLast(1).on('child_added', function(data) {
+	// get table entry array
+	var rows = Object.values(data.val())
+	// get first and only entry in rows
+	var row = rows[0]
+	// deconstruct row properties
 	var name = row.name;
 	var role = row.role;
 	var start = row.startDate;
@@ -109,5 +116,6 @@ database.ref('employees').orderByChild('dateAdded').limitToLast(1).on('value', f
 		monthlyRate,
 		billed
 	)
+	// push new row to table
 	$('#table').append(rowHtml)
 })
